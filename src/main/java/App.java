4dec9be22570cac5ee83d7de06457fadf51d8d43
deeksonparlma <dicksonparlma@gmail.com>
2019-04-12 14:34:50 +0300

    support files

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,14 @@
-import static spark.Spark.setPort;
-import static spark.Spark.staticFileLocation;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
-        staticFileLocation("/public");
+        staticFileLocation("resources/public");
         ProcessBuilder process = new ProcessBuilder();
         Integer port;
         if (process.environment().get("PORT") != null) {
@@ -12,6 +17,14 @@ public class App {
             port = 4567;
         }
 
-        setPort(port);
+        //layout template as a string//
+        String layout = "templates/layout.vtl";
+
+        //routes//
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template", "templates/index.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
     }
 }
