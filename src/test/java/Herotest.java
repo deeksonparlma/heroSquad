a408e98c00bc50617ade48e999d9fe313bbccdd6
deeksonparlma <dicksonparlma@gmail.com>
2019-04-15 10:45:25 +0300

    Hero test added

--- src/test/java/Herotest.java
+++ src/test/java/Herotest.java
@@ -3,12 +3,14 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class Herotest {
-    public class TaskTest {
-
-//        @Test
-//        public void Task_instantiatesCorrectly_true() {
-//            Hero myTask = new Hero("Mow the lawn");
-//            assertEquals(true, true);
-//        }
+        @Test
+        public void Hero_gets_name(){
+            Hero hero = new Hero("Dickson", "Catch pick-pocketers");
+            assertEquals("Dickson", hero.getName());
+        }
+    @Test
+    public void Hero_gets_cause(){
+        Hero hero = new Hero("Dickson", "Catch pick-pocketers");
+        assertEquals("Catch pick-pocketers", hero.getCause());
     }
 }
